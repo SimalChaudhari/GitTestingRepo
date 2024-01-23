598d8452c8121e11725796741bd93b3295befcbd
SimalChaudhari <simalchaudhari20496@gmail.com>
2024-01-23 12:40:40 +0530

    add amination for header

---
+++ assets/Js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Add animation class to header on page load
+    const animatedHeader = document.getElementById('animatedHeader');
+    animatedHeader.classList.add('animate-header');
+  });
+  
