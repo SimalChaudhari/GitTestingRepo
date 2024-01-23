49d361ae10a634f631c4420ba6484c2a99af0896
SimalChaudhari <simalchaudhari20496@gmail.com>
2024-01-23 13:52:12 +0530

    add animation for hero text

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Add animation class to header on page load
     const animatedHeader = document.getElementById('animatedHeader');
+    const animatedHeroLeft = document.getElementById('animatedHeroLeft');
+    const animatedHeroRight = document.getElementById('animatedHeroRight');
     animatedHeader.classList.add('animate-header');
+    animatedHeroLeft.classList.add('animate-hero-left');
+    animatedHeroRight.classList.add('animate-hero-right');
   });
   
