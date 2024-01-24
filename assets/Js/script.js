deea4c058601232a831e76655e09c7dcaa8f1cc3
SimalChaudhari <simalchaudhari20496@gmail.com>
2024-01-24 14:46:28 +0530

    commit

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,10 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Add animation class to header on page load
-    const animatedHeader = document.getElementById('animatedHeader');
-    const animatedHeroLeft = document.getElementById('animatedHeroLeft');
-    const animatedHeroRight = document.getElementById('animatedHeroRight');
-    animatedHeader.classList.add('animate-header');
-    animatedHeroLeft.classList.add('animate-hero-left');
-    animatedHeroRight.classList.add('animate-hero-right');
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  // Add animation class to header on page load
+  const animatedHeader = document.getElementById("animatedHeader");
+  const animatedHeroLeft = document.getElementById("animatedHeroLeft");
+  const animatedHeroRight = document.getElementById("animatedHeroRight");
+  animatedHeader.classList.add("animate-header");
+  animatedHeroLeft.classList.add("animate-hero-left");
+  animatedHeroRight.classList.add("animate-hero-right");
+});
+
+function toggleSlider() {
+  var slider = document.getElementById("slider");
+  slider.classList.toggle("slider_opened");
+}
